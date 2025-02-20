bcb6eb322d6249f58651ad1ad37bef5e1adfba24
Shatlin <shatlin.denistan@takealot.com>
2025-02-20 11:17:28 +0200

    first test

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,5 +1,28 @@
+import pytest
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
 class TestLogin:
-    def test_login(self):
-        a = 1
-        b = 2
-        assert a
+    @pytest.mark.login
+    def test_login(self, playwright: Playwright):
+        browser = playwright.chromium.launch(headless=False)
+        admin_context = browser.new_context()
+        page = admin_context.new_page()
+        user_context = browser.new_context()
+        user_page = user_context.new_page()
+        page.goto("https://retail.master.env/")
+        user_page.goto("https://retail.master.env/")
+
+        page.get_by_role("textbox", name="Email").fill("shatlin.denistan@takealot.com")
+        page.get_by_role("textbox", name="Password").fill("wrongpassword")
+        page.get_by_role("button", name="Login").nth(1).click()
+
+        user_page.get_by_role("textbox", name="Email").fill("shatlin.denistan@takealot.com")
+        user_page.get_by_role("textbox", name="Password").fill("wrongpassword")
+        user_page.get_by_role("button", name="Login").nth(1).click()
+
+        # ---------------------
+        admin_context.close()
+        user_context.close()
+        browser.close()
